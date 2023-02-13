7f6a1f358c6dd4ac4712fc0b4f9ecc51fd98c70e
Beste Kocaoğlu <78082138+besteko@users.noreply.github.com>
2023-02-13 11:51:02 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Beste Kocaoglu on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
